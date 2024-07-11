7df54a04dfdd5dcc6af50bf76a0121d1905c69f3
citycrasher <44635663+citycrasher@users.noreply.github.com>
2024-07-11 23:47:32 +0530

    Update content for indian movie

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-   from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for
 import logging
 import api, settings
 app = Flask(__name__)
