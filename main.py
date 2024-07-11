c3d7360ef9bcfdeac20bfea26b7fdb028b2a2fa3
citycrasher <44635663+citycrasher@users.noreply.github.com>
2024-07-12 00:26:47 +0530

    Update the port to 9002

--- main.py
+++ main.py
@@ -64,4 +64,4 @@ def submit():
 
 if __name__ == '__main__':
     # app.run(debug=True, port=8001)
-    app.run(debug= True, host='0.0.0.0', port=8002)
+    app.run(debug= True, host='0.0.0.0', port=9002)
